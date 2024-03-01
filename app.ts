0538c1c8e62a30ea8010c7d2782120c3069061a2
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-03-01 16:59:38 +0100

    The modified typescript

--- app.ts
+++ app.ts
@@ -1,29 +1,25 @@
-// list storage and display task list
-let tasks: string[] = [];
-let taskList = document.getElementById('taskList') as HTMLLIElement;
+// Acquiring the element
+const taskList = document.getElementById('taskList') as HTMLHtmlElement;
 const taskInput = document.getElementById('taskInput') as HTMLInputElement;
 
-
-
+// List storage in an empty Array
+let tasks: string[] = [];
 
 // Function to add task
 function addTask() {
         const task = taskInput.value.trim();    //trim removes white spaces
     if (task !== "") {
         tasks.push(task);
-        // displayTasks();
+        displayTasks();
         taskInput.value = "";
-    } else {
-        alert('Please enter a task');
     }
 }
-
 // // Function to modify
 function modifyTask(num: number) {
     const newTask = prompt('Enter the new task', tasks[num]);
     if (newTask !== null) {
-        tasks[num] = newTask.trim();
-        // displayTasks();
+        tasks.splice(num, 1, newTask)
+        displayTasks();
     }
 }
 
@@ -31,15 +27,20 @@ function modifyTask(num: number) {
 function deleteTask(num: number) {
     if (confirm("Are you sure you want to delete?")) {
         tasks.splice(num, 1)
-        // displayTasks()
+        displayTasks()
     }
 }
 
 // // Function to display Tasks
 function displayTasks(){
-    taskList.innerHTML = "";
-    tasks.forEach((task, num) => {
-        const li = document.createElement('li');
-    })
+    let displayList: string = '';
+    for(let index:number = 0; index<tasks.length; index++){
+        displayList += `<li>${tasks[index]} <button onclick="modifyTask(${index})">Edit</button> <button onclick="deleteTask(${tasks[index]})">CompleteTask</button></li>`
+    };
+    taskList.innerHTML = displayList
+    // taskList.innerHTML = "";
+    // tasks.forEach((task, num) => {
+    //     const li = document.createElement('li');
+    // })
 }
 // displayTasks()
